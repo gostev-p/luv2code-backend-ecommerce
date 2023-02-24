89298407c7655dd39210486a19e28a024f7e7775
Gostev, Philipp <philipp.gostev@adesso.de>
2023-02-24 08:59:06 +0100

    Release 3.0 (unfinished)
    - made unique email for customers when ordering
    - fixed the security leak
      -> CustomerRepository can't be exposed

--- src/main/java/com/udemy/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/udemy/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,7 @@ import com.udemy.ecommerce.entity.State;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -20,7 +21,10 @@ import java.util.Set;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private EntityManager entityManager;
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
+
+    private final EntityManager entityManager;
 
     @Autowired
     public MyDataRestConfig(EntityManager entityManager){
@@ -29,18 +33,22 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
-        // disable HTTP methods for Product: PUT, POST and DELETE
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
         disableHttpMethods(Product.class, config, theUnsupportedActions);
         disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
         disableHttpMethods(Country.class, config, theUnsupportedActions);
         disableHttpMethods(State.class, config, theUnsupportedActions);
 
         exposeIds(config);
+
+        // configure cors mapping
+        cors.addMapping( config.getBasePath() + "/**").allowedOrigins(allowedOrigins);
     }
 
-    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+    private void disableHttpMethods(Class<?> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
         config.getExposureConfiguration()
                 .forDomainType(theClass)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
@@ -54,15 +62,15 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         Set<EntityType<?>> entityTypes = entityManager.getMetamodel().getEntities();
 
         // create an array of the entity types
-        List<Class> entityClasses = new ArrayList<>();
+        List<Class<?>> entityClasses = new ArrayList<>();
 
         // get the entity types for the entities
-        for(EntityType tempEntityType: entityTypes){
+        for(EntityType<?> tempEntityType: entityTypes){
             entityClasses.add(tempEntityType.getJavaType());
         }
 
         // expose the entity ids for the array of entity/domain types
-        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        Class<?>[] domainTypes = entityClasses.toArray(new Class[0]);
         configuration.exposeIdsFor(domainTypes);
     }
 }
