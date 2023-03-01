b40296e6908e8c1912feb8b2878f05ff078da371
Gostev, Philipp <philipp.gostev@adesso.de>
2023-03-01 17:45:50 +0100

    Release 3.0
    - added stripe api

--- src/main/java/com/udemy/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/udemy/ecommerce/controller/CheckoutController.java
@@ -1,15 +1,23 @@
 package com.udemy.ecommerce.controller;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.udemy.ecommerce.dto.PaymentInfo;
 import com.udemy.ecommerce.dto.Purchase;
 import com.udemy.ecommerce.dto.PurchaseResponse;
 import com.udemy.ecommerce.service.CheckoutService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/checkout")
 public class CheckoutController {
 
+    private final Logger logger = Logger.getLogger(getClass().getName());
     private final CheckoutService checkoutService;
 
     @Autowired
@@ -20,5 +28,16 @@ public class CheckoutController {
     @PostMapping("/purchase")
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
         return checkoutService.placeOrder(purchase);
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        logger.info("paymentInfo.amount: " + paymentInfo.getAmount());
+
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
 }
--- src/main/java/com/udemy/ecommerce/service/CheckoutService.java
+++ src/main/java/com/udemy/ecommerce/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package com.udemy.ecommerce.service;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.udemy.ecommerce.dto.PaymentInfo;
 import com.udemy.ecommerce.dto.Purchase;
 import com.udemy.ecommerce.dto.PurchaseResponse;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
